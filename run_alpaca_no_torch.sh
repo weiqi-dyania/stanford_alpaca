546d5d9d48dc6316437a2693e849beeae09980ed
Weiqi Sun <weiqi@dyaniahealth.com>
2023-04-04 22:40:42 -0400

    add scripts to process data

---
+++ run_alpaca_no_torch.sh
@@ -0,0 +1,20 @@
+python3 train.py \
+    --model_name_or_path weiqis/alpaca-7b \
+    --data_path ./alpaca_data_sample.json \
+    --bf16 True \
+    --output_dir ./output_dir_llama_test \
+    --num_train_epochs 3 \
+    --per_device_train_batch_size 2 \
+    --per_device_eval_batch_size 2 \
+    --gradient_accumulation_steps 1 \
+    --evaluation_strategy "no" \
+    --save_strategy "epoch" \
+    --save_total_limit 3 \
+    --learning_rate 2e-5 \
+    --weight_decay 0. \
+    --warmup_ratio 0.03 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 1 \
+    --fsdp "full_shard auto_wrap" \
+    --fsdp_config "fsdp_config.json" \
+    --tf32 True
