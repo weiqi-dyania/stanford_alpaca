f6691bba21c0b7f09f434ee681be5f5740bc55ab
Weiqi Sun <weiqi@dyaniahealth.com>
2023-04-18 22:32:39 -0400

    add formatter for training data

---
+++ alpaca_data_formater_train.py
@@ -0,0 +1,36 @@
+import sys
+import json
+
+
+PROMPT_DICT = {
+    "prompt_input": (
+        "Below is an instruction that describes a task, paired with an input that provides further context. "
+        "Write a response that appropriately completes the request.\n\n"
+        "### Instruction:\n{instruction}\n\n### Input:\n{input}\n\n### Response:"
+    ),
+    "prompt_no_input": (
+        "Below is an instruction that describes a task. "
+        "Write a response that appropriately completes the request.\n\n"
+        "### Instruction:\n{instruction}\n\n### Response:"
+    ),
+}
+
+def main(input_file, output_file):
+    prompt_input, prompt_no_input = PROMPT_DICT["prompt_input"], PROMPT_DICT["prompt_no_input"]
+
+    with open(input_file) as f:
+        list_data_dict = json.load(f)
+
+    sources = [
+        prompt_input.format_map(example) if example.get("input", "").strip() != "" else prompt_no_input.format_map(example)
+        for example in list_data_dict
+    ]
+    targets = [f"{example['output']}" for example in list_data_dict]
+
+    with open(output_file, "w") as f:
+        for s, t in zip(sources, targets):
+            f.write(json.dumps({"prompt": s, "completion": t}))
+            f.write("\n")
+
+if __name__ == "__main__":
+    main(sys.argv[1], sys.argv[2])
